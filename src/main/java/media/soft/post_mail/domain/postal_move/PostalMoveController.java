1beeaf2032c577adba1b0c9cc4e3533d5b4c0108
Marina Nesterenko <78105886+nesterenko-marina99@users.noreply.github.com>
2023-06-07 10:37:22 +0400

    Update PostalMoveController.java

--- src/main/java/media/soft/post_mail/domain/postal_move/PostalMoveController.java
+++ src/main/java/media/soft/post_mail/domain/postal_move/PostalMoveController.java
@@ -2,7 +2,6 @@ package media.soft.post_mail.domain.postal_move;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -15,22 +14,30 @@ public class PostalMoveController {
     private final PostalMoveService postalMoveService;
 
     @PostMapping(value = "/create")
-    public ResponseEntity<PostalMove> registerNewMailingAtOffice(@Valid @RequestBody PostalMove postalMove) {
-        return new ResponseEntity<>(postalMoveService.registerFirstMove(postalMove), HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public PostalMove registerNewMailingAtOffice(@Valid @RequestBody PostalMove postalMove) {
+        return postalMoveService.registerFirstMove(postalMove);
     }
 
     @PostMapping(value = "/departure")
-    public ResponseEntity<PostalMove> registerDeparture(@Valid @RequestBody PostalMove postalMove) {
-        return new ResponseEntity<>(postalMoveService.registerDeparture(postalMove), HttpStatus.ACCEPTED);
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseBody
+    public PostalMove registerDeparture(@Valid @RequestBody PostalMove postalMove) {
+        return postalMoveService.registerDeparture(postalMove);
     }
 
     @PostMapping(value = "/arrival")
-    public ResponseEntity<PostalMove> registerArrival(@Valid @RequestBody PostalMove postalMove) {
-        return new ResponseEntity<>(postalMoveService.registerArrival(postalMove), HttpStatus.ACCEPTED);
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseBody
+    public PostalMove registerArrival(@Valid @RequestBody PostalMove postalMove) {
+        return postalMoveService.registerArrival(postalMove);
     }
 
     @PostMapping(value = "/issue")
-    public ResponseEntity<PostalMove> issueMailing(@Valid @RequestBody PostalMove postalMove) {
-        return new ResponseEntity<>(postalMoveService.issuanceOfMailingToAddressee(postalMove), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public PostalMove issueMailing(@Valid @RequestBody PostalMove postalMove) {
+        return postalMoveService.issuanceOfMailingToAddressee(postalMove);
     }
 }
